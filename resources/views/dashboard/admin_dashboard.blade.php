ca67946703044d4454702c231ee9e87dc0450c4e
Nuno Gomes <2140624@my.ipleiria.pt>
2017-04-20 10:29:04 +0100

    Refactor on master.blade

--- resources/views/dashboard/admin_dashboard.blade.php
+++ resources/views/dashboard/admin_dashboard.blade.php
@@ -5,41 +5,32 @@
 
 <div class="container">
 	    <a class="btn btn-primary" href="{{url('/all_users')}}">Todos os utilizadores</a>
-	    <div class="pull-right"> </div>
 
-<br>
+<br /><br />
 
 	    <a class="btn btn-primary" href="{{url('/admins')}}">Administradores</a>
-	    <div class="pull-right"> </div>
 
-<br>
+<br /><br />
 
 	    <a class="btn btn-primary" href="{{url('/healthcarepros')}}">Profissionais de saude</a>
-	    <div class="pull-right"> </div>
 
-<br>
+<br /><br />
 
 
 	    <a class="btn btn-primary" href="{{route('admin.admin_caregivers')}}">Cuidadores</a>
-	    <div class="pull-right"> 
 
-<br>
-
+<br /><br />
 
 	    <a class="btn btn-primary" href="{{url('/patients')}}">Pacientes</a>
-	    <div class="pull-right"> </div>
 
-<br>
-
+<br /><br />
 
 	    <a class="btn btn-primary" href="{{url('/needs')}}">Necessidades</a>
-	    <div class="pull-right"> </div>
 
-<br>
-
+<br /><br />
 
 	    <a class="btn btn-primary" href="{{url('/materials')}}">Materiais</a>
-	    <div class="pull-right"> </div>
+
 </div>
 	
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,37 +1,90 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="{{ config('app.locale') }}">
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('tittle')</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Bootstrap core CSS -->
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <title>Healthcare Management</title>
 
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="/css/index.css" rel="stylesheet">
-  </head>
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
 
-  <body>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
 
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        Healthcare Management
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')   
     
-    
-  	@include('layouts.nav')
+        <br /><br />
 
-    <br><br>
+        @include('layouts.footer')
+    </div>
 
-    @yield('content')
-    
-    
-    <br><br>    
-    
-    @include('layouts.footer')
-  
-
-  </body>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
 </html>
