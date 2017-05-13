916047648cb291d0dbc96bb2b467ce1f6a389cb3
Nuno Gomes <2140624@my.ipleiria.pt>
2017-05-13 12:45:07 +0100

    Fixed method that return patients json

--- app/Http/Controllers/CaregiversController.php
+++ app/Http/Controllers/CaregiversController.php
@@ -162,270 +162,112 @@ class CaregiversController extends Controller
             return response('Não Autorizado', 401);
         }
 
-        //erro k aparecia era por causa de perder o array caso faça forget
-        $c = collect();
+        $patientsCollection = collect();
         $patients = $user->patients;
         foreach ($patients as $patient) {
             $objectP = new \stdClass();
-            $objectP->id = $patient->id;
-            $objectP->email = $patient->email;
-            $objectP->name = $patient->name;
-            $objectP->location = $patient->location;
-            $objectP->caregiver_id = $patient->caregiver_id;
-            $objectP->created_by = $patient->created_by;
-            $objectP->created_at = (string)$patient->created_at;
-            $objectP->updated_at = (string)$patient->updated_at;
-            $objectP->needs = [];
+            $this->buildPatient($objectP, $patient);
             foreach ($patient->needs as $need) {
                 $objectN = new \stdClass();
-                $objectN->id = $need->id;
-                $objectN->description = $need->description;
-                $objectN->created_by = $need->created_by;
-                $objectN->created_at = (string)$need->created_at;
-                $objectN->updated_at = (string)$need->updated_at;
-                $objectN->materials = [];
+                $this->buildNeed($objectN, $need);
                 foreach ($need->materials as $material) {
                     if ($material->blocked != 1) {
                         $objectM = new \stdClass();
-                        $objectM->id = $material->id;
-                        $objectM->type = $material->type;
-                        $objectM->description = $material->description;
-                        $objectM->name = $material->name;
+                        $this->buildMaterial($objectM, $material);
 
-                        switch ($material->type) {
-                            case 'text':
-                                $objectM->body = $material->body;
-                                break;
-
-                            case 'image':
-                                $objectM->url = $material->url;
-                                $objectM->path = $material->path;
-                                $objectM->mime = $material->mime;
-                                break;
-
-                            case 'video':
-                                $objectM->url = $material->url;
-                                $objectM->path = $material->path;
-                                $objectM->mime = $material->mime;
-                                break;
-
-                            case 'annex':
-                                if ($material->url != null) {
-                                    $objectM->url = $material->url;
-                                }
-
-                                if ($material->path != null && $material->mime != null) {
-                                    $objectM->path = $material->path;
-                                    $objectM->mime = $material->mime;
-                                }
-                                break;
-
-                            case 'emergencyContact':
-                                $objectM->number = $material->number;
-                                break;
-
-                            default:
-                                break;
-                        }
-
-                        $objectM->created_by = $material->created_by;
-                        $objectM->created_at = (string)$material->created_at;
-                        $objectM->updated_at = (string)$material->updated_at;
-
-                        if ($material->type == 'composite') {
-                            $objectM->materials = [];
-                            $compositeMaterials = $material->materials()->withPivot('order')->orderBy('pivot_order', 'asc')->get();
-                            foreach ($compositeMaterials as $compositeMaterial) {
-                                $objectCM = new \stdClass();
-                                $objectCM->id = $compositeMaterial->id;
-                                $objectCM->type = $compositeMaterial->type;
-                                $objectCM->description = $compositeMaterial->description;
-                                $objectCM->name = $compositeMaterial->name;
-
-                                switch ($compositeMaterial->type) {
-                                    case 'text':
-                                        $objectCM->body = $compositeMaterial->body;
-                                        break;
-
-                                    case 'image':
-                                        $objectCM->url = $compositeMaterial->url;
-                                        $objectCM->path = $compositeMaterial->path;
-                                        $objectCM->mime = $compositeMaterial->mime;
-                                        break;
-
-                                    case 'video':
-                                        $objectCM->url = $compositeMaterial->url;
-                                        $objectCM->path = $compositeMaterial->path;
-                                        $objectCM->mime = $compositeMaterial->mime;
-                                        break;
-
-                                    case 'annex':
-                                        if ($compositeMaterial->url != null) {
-                                            $objectCM->url = $compositeMaterial->url;
-                                        }
-
-                                        if ($compositeMaterial->path != null && $compositeMaterial->mime != null) {
-                                            $objectCM->path = $compositeMaterial->path;
-                                            $objectCM->mime = $compositeMaterial->mime;
-                                        }
-                                        break;
-
-                                    case 'emergencyContact':
-                                        $objectCM->number = $compositeMaterial->number;
-                                        break;
-
-                                    default:
-                                        break;
-                                }
-
-                                $objectCM->created_by = $compositeMaterial->created_by;
-                                $objectCM->created_at = (string)$compositeMaterial->created_at;
-                                $objectCM->updated_at = (string)$compositeMaterial->updated_at;
-
-                                array_push($objectM->materials, $objectCM);
-                            }
-                        }
                         array_push($objectN->materials, $objectM);
                     }
                 }
                 array_push($objectP->needs, $objectN);
             }
-            $c->push($objectP);
+            $patientsCollection->push($objectP);
         }
 
-        return response()->json($c);      
+        return response()->json($patientsCollection);      
     }
 
-    public function caregiversMaterialsAPI(Request $request, $id)
+    private function buildPatient($objectP, $patient)
     {
-        $caregiver_token = $request->header('Authorization');
-        $caregiver = Caregiver::find($id);
+        $objectP->id = $patient->id;
+        $objectP->email = $patient->email;
+        $objectP->name = $patient->name;
+        $objectP->location = $patient->location;
+        $objectP->caregiver_id = $patient->caregiver_id;
+        $objectP->created_by = $patient->created_by;
+        $objectP->created_at = (string)$patient->created_at;
+        $objectP->updated_at = (string)$patient->updated_at;
+        $objectP->needs = [];
+    }
 
-        if ($caregiver == null) {
-           return response('Não Encontrado', 404);
+    private function buildNeed($objectN, $need)
+    {
+        $objectN->id = $need->id;
+        $objectN->description = $need->description;
+        $objectN->created_by = $need->created_by;
+        $objectN->created_at = (string)$need->created_at;
+        $objectN->updated_at = (string)$need->updated_at;
+        $objectN->materials = [];
+    }
+
+    private function buildMaterial($objectM, $material)
+    {
+        $objectM->id = $material->id;
+        $objectM->type = $material->type;
+        $objectM->description = $material->description;
+        $objectM->name = $material->name;
+
+        switch ($material->type) {
+            case 'text':
+                $objectM->body = $material->body;
+                break;
+
+            case 'image':
+                $objectM->url = $material->url;
+                $objectM->path = $material->path;
+                $objectM->mime = $material->mime;
+                break;
+
+            case 'video':
+                $objectM->url = $material->url;
+                $objectM->path = $material->path;
+                $objectM->mime = $material->mime;
+                break;
+
+            case 'annex':
+                if ($material->url != null) {
+                    $objectM->url = $material->url;
+                }
+
+                if ($material->path != null && $material->mime != null) {
+                    $objectM->path = $material->path;
+                    $objectM->mime = $material->mime;
+                }
+                break;
+
+            case 'emergencyContact':
+                $objectM->number = $material->number;
+                break;
+
+            default:
+                break;
         }
 
-        if (!$caregiver_token || $caregiver->caregiver_token != $caregiver_token) {
-            return response('Não Autorizado', 401);
-        }
+        $objectM->created_by = $material->created_by;
+        $objectM->created_at = (string)$material->created_at;
+        $objectM->updated_at = (string)$material->updated_at;
 
-        $materials = [];
-        $patients = $caregiver->patients;
-        foreach ($patients as $p) {
-            $needs = $p->needs;
+        if ($material->type == 'composite') {
+            $objectM->materials = [];
+            $compositeMaterials = $material->materials()->withPivot('order')->orderBy('pivot_order', 'asc')->get();
+            foreach ($compositeMaterials as $compositeMaterial) {
+                if ($compositeMaterial->blocked != 1) {
+                    $objectCM = new \stdClass();
+                    $this->buildMaterial($objectCM, $compositeMaterial);
 
-            foreach ($needs as $n) {
-                $n_materials = $n->materials; 
-                
-                foreach ($n_materials as $m) {
-
-                    $material_exists = $this->materialExists($materials, $m);
-
-                    if (!$material_exists) {
-                        array_push($materials, $m);
-                    }
+                    array_push($objectM->materials, $objectCM);
                 }
             }
         }
-
-        return response()->json($materials);      
-    }
-
-    public function patientsMaterials(Request $request, $caregiver_id, $patient_id)
-    {
-        $caregiver_token = $request->header('Authorization');
-        $caregiver = Caregiver::find($caregiver_id);
-        $patient = Patient::find($patient_id);
-
-        if ($caregiver == null || $patient == null) {
-           return response('Não Encontrado', 404);
-        }
-
-        $caregivers_patient = false;
-        foreach ($caregiver->patients as $p) {
-            if ($p == $patient) {
-                $caregivers_patient = true;
-                break;
-            }
-        }
-
-        if (!$caregiver_token || $caregiver->caregiver_token != $caregiver_token || !$caregivers_patient) {
-            return response('Não Autorizado', 401);
-        }
-
-        $materials = [];
-        foreach ($patient->needs as $n) {
-            $n_materials = $n->materials; 
-                
-            foreach ($n_materials as $m) {
-                $material_exists = $this->materialExists($materials, $m);
-
-                if (!$material_exists) {
-                    array_push($materials, $m);
-                }
-            }
-        }
-        
-        return response()->json($materials);
-    }
-
-    public function patientsNeeds(Request $request, $caregiver_id, $patient_id)
-    {
-        $caregiver_token = $request->header('Authorization');
-        $caregiver = Caregiver::find($caregiver_id);
-        $patient = Patient::find($patient_id);
-
-        if ($caregiver == null || $patient == null) {
-           return response('Não Encontrado', 404);
-        }
-
-        $caregivers_patient = false;
-        foreach ($caregiver->patients as $p) {
-            if ($p == $patient) {
-                $caregivers_patient = true;
-                break;
-            }
-        }
-
-        if (!$caregiver_token || $caregiver->caregiver_token != $caregiver_token || !$caregivers_patient) {
-            return response('Não Autorizado', 401);
-        }
-        
-        return response()->json($patient->needs);
-    }
-
-    public function patientsNeedsMaterials(Request $request, $caregiver_id, $patient_id, $need_id)
-    {
-        $caregiver_token = $request->header('Authorization');
-        $caregiver = Caregiver::find($caregiver_id);
-        $patient = Patient::find($patient_id);
-        $need = Need::find($need_id);
-
-        if ($caregiver == null || $patient == null || $need == null) {
-           return response('Não Encontrado', 404);
-        }
-
-        $caregivers_patient = false;
-        foreach ($caregiver->patients as $p) {
-            if ($p == $patient) {
-                $caregivers_patient = true;
-                break;
-            }
-        }
-
-        $patients_need = false;
-        foreach ($patient->needs as $n) {
-            if ($n->id == $need->id) {
-                $patients_need = true;
-                break;
-            }
-        }
-
-        if (!$caregiver_token || $caregiver->caregiver_token != $caregiver_token || !$caregivers_patient || !$patients_need) {
-            return response('Não Autorizado', 401);
-        }
-        
-        return response()->json($need->materials);
     }
 
     public function proceedings(Request $request, $caregiver_id)
@@ -442,19 +284,6 @@ class CaregiversController extends Controller
         }
 
         return response()->json($caregiver->proceedings);
-    }
-
-    private function materialExists($materials, $m)
-    {
-        $material_exists = false;
-        foreach ($materials as $material) {
-            if ($m->id == $material->id) {
-                $material_exists = true;
-                break;
-            }
-        }
-
-        return $material_exists;
     }
 
     private function cmp($a, $b)
